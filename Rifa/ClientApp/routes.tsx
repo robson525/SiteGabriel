c87df2048871b05370bd8dbf4e156a3237d156c8
robson.silva <robson.c.silva@capgemini.com>
2021-03-12 12:12:58 -0300

    Excluindo arquivos desnecessarios

--- Rifa/ClientApp/routes.tsx
+++ Rifa/ClientApp/routes.tsx
@@ -4,13 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { Edit } from "./components/Edit";
 
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
-
 export const routes = <Layout>
     <Route exact path='/' component={ Home } />
     <Route path='/edit/:id' component={ Edit } />
-
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
 </Layout>;
